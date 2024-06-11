14b40c42cc730eb182b52f455227fb375f2cc168
kauzarc <julien.rolland@kauzarc.fr>
2024-06-11 12:01:56 +0200

    requirements

--- src/pl_module.py
+++ src/pl_module.py
@@ -2,6 +2,8 @@ from pytorch_lightning import LightningModule
 from torch import FloatTensor
 from transformers import BartConfig, PreTrainedTokenizerFast, BartForConditionalGeneration  # type: ignore
 from transformers.modeling_outputs import Seq2SeqLMOutput  # type: ignore
+from transformers.optimization import AdamW
+from omegaconf import OmegaConf
 
 from data import Batch
 from utils import shift_tokens_left
@@ -10,12 +12,14 @@ from utils import shift_tokens_left
 class PLModule(LightningModule):
     def __init__(
         self,
+        omegaconf: OmegaConf,
         config: BartConfig,
         tokenizer: PreTrainedTokenizerFast,
         model: BartForConditionalGeneration,
     ):
         super().__init__()
 
+        self.save_hyperparameters(omegaconf)
         self.config = config
         self.tokenizer = tokenizer
         self.model = model
@@ -37,5 +41,28 @@ class PLModule(LightningModule):
         }
 
         outputs: Seq2SeqLMOutput = self.model(**inputs)
-        self.log("loss", outputs.loss)
+        self.log("train_loss", outputs.loss)
         return outputs.loss  # type: ignore
+
+    def configure_optimizers(self) -> AdamW:
+        no_decay = ["bias", "LayerNorm.weight"]
+        optimizer_grouped_parameters = [
+            {
+                "params": [
+                    param
+                    for name, param in self.model.named_parameters()
+                    if not any(sub_string in name for sub_string in no_decay)
+                ],
+                "weight_decay": self.hparams.weight_decay,
+            },
+            {
+                "params": [
+                    param
+                    for name, param in self.model.named_parameters()
+                    if any(sub_string in name for sub_string in no_decay)
+                ],
+                "weight_decay": 0.0,
+            },
+        ]
+
+        return AdamW(optimizer_grouped_parameters)  # type: ignore
