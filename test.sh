a9c114ab435b87dfda30a5f175a6a9ddbdb7cfb3
Julien Rolland <julien.rolland@universite-paris-saclay.fr>
2024-07-11 16:10:33 +0200

    test.sh

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --partition=testing
+#SBATCH --qos=debug
+#SBATCH --job-name=real_test
+#SBATCH --output=logs/test.txt
+#SBATCH --gres=gpu:1
+#
+#SBATCH --mail-user=julien.rolland@universite-paris-saclay.fr
+#SBATCH --mail-type=ALL
+
+source .venv/bin/activate
+srun python train.py
